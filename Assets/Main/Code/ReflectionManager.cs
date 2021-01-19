d9cbf4e3f68cec1baa5e66bdb3dea4c08616da99
Semimartian <semimartian@hotmail.com>
2021-01-19 15:35:19 +0200

    Floor reflecctions, cute models, ceiling

--- Assets/Main/Code/ReflectionManager.cs
+++ Assets/Main/Code/ReflectionManager.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class ReflectionManager : MonoBehaviour
 {
-    private Camera reflectionCamera;
+    [SerializeField] private Camera reflectionCamera;
     private Camera mainCamera;
    [SerializeField] private GameObject reflectionPlane;
     RenderTexture renderTarget;
@@ -12,21 +13,19 @@ public class ReflectionManager : MonoBehaviour
     [Range(0,1)]
     [SerializeField] private float reflectionFactor = 0.5f;
 
-    // Start is called before the first frame update
+    //Cache:
+    Transform mainCameraTransform;
+    Transform reflectionPlaneTransform;
+    Transform reflectionCameraTransform;
+
     void Start()
     {
-        GameObject reflectionCameraGO = new GameObject("ReflectionCamera");
-        reflectionCamera = reflectionCameraGO.AddComponent<Camera>();
-        reflectionCamera.enabled = false;
-
-        mainCamera = Camera.main;
-
-        renderTarget = new RenderTexture(Screen.width, Screen.height, 24);
+        InitialiseReflectionCamera();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //TODO: I don't like:
         Shader.SetGlobalFloat("_reflectionFactor",reflectionFactor);
     }
 
@@ -35,16 +34,43 @@ public class ReflectionManager : MonoBehaviour
         RenderReflection();
     }
 
+    private void InitialiseReflectionCamera()
+    {
+        mainCamera = Camera.main;
+
+        /*GameObject reflectionCameraGO = new GameObject("ReflectionCamera");
+        reflectionCamera = reflectionCameraGO.AddComponent<Camera>();*/
+        reflectionCamera.enabled = false;
+        //reflectionCamera.CopyFrom(mainCamera);
+
+        /*PostProcessLayer mainCameraPostProcessLayer = mainCamera.GetComponent<PostProcessLayer>();
+        if (mainCameraPostProcessLayer != null)
+        {
+            //Copy component...
+            PostProcessLayer reflectionCameraPostProcessLayer = reflectionCameraGO.AddComponent<PostProcessLayer>();
+            reflectionCameraPostProcessLayer.volumeTrigger = mainCameraPostProcessLayer.volumeTrigger;
+            reflectionCameraPostProcessLayer.volumeLayer = mainCameraPostProcessLayer.volumeLayer;
+            reflectionCameraPostProcessLayer.antialiasingMode = mainCameraPostProcessLayer.antialiasingMode;
+            reflectionCameraPostProcessLayer.stopNaNPropagation = mainCameraPostProcessLayer.stopNaNPropagation;
+        }*/
+        //NOTE: might be a bgood idea to play around with the resolution 
+        renderTarget = new RenderTexture(Screen.width, Screen.height, 24);
+
+        reflectionCamera.targetTexture = renderTarget;
+
+        mainCameraTransform = mainCamera.transform;
+        reflectionPlaneTransform = reflectionPlane.transform;
+        reflectionCameraTransform = reflectionCamera.transform;
+
+    }
+
     private void RenderReflection()
     {
-        reflectionCamera.CopyFrom(mainCamera);
-        Transform mainCameraTransform = mainCamera.transform;
+       // reflectionCamera.CopyFrom(mainCamera);
 
         Vector3 cameraDirectionWorldSpace = mainCameraTransform.forward;
         Vector3 cameraUpWorldSpace = mainCameraTransform.up;
         Vector3 cameraPositionWorldSpace = mainCameraTransform.position;
-
-        Transform reflectionPlaneTransform = reflectionPlane.transform;
 
         Vector3 cameraDirectionPlaneSpace = reflectionPlaneTransform.InverseTransformDirection(cameraDirectionWorldSpace);
         Vector3 cameraUpPlaneSpace = reflectionPlaneTransform.InverseTransformDirection(cameraUpWorldSpace);
@@ -58,12 +84,8 @@ public class ReflectionManager : MonoBehaviour
         cameraUpWorldSpace = reflectionPlaneTransform.TransformDirection(cameraUpPlaneSpace);
         cameraPositionWorldSpace = reflectionPlaneTransform.TransformPoint(cameraPositionPlaneSpace);
 
-        Transform reflectionCameraTransform = reflectionCamera.transform;
-
         reflectionCameraTransform.position = cameraPositionWorldSpace;
         reflectionCameraTransform.LookAt(cameraPositionWorldSpace + cameraDirectionWorldSpace, cameraUpWorldSpace);
-
-        reflectionCamera.targetTexture = renderTarget;
 
         reflectionCamera.Render();
 
